ca3f7efa6f2c4067576a92e0a3a57d2f779ea022
egonrex <eg.melo@duocuc.cl>
2026-05-17 22:47:14 -0400

    Delete proyecto tienda-te/proyecto tienda-te directory

--- proyecto tienda-te/proyecto tienda-te/tienda-te-ventas/src/main/java/com/hospital_vm_cl/hospital_vm_atencion/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hospital_vm_cl.hospital_vm_atencion.exceptions;
-
-// Excepción para recursos que no existen
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- proyecto tienda-te/proyecto tienda-te/tienda-te-ventas/src/main/java/com/hospital_vm_cl/hospital_vm_atencion/exceptions/RemoteServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.hospital_vm_cl.hospital_vm_atencion.exceptions;
-// Excepción para errores al consumir otro microservicio
-public class RemoteServiceException extends RuntimeException {
-    public RemoteServiceException(String message) {
-        super(message);
-    }
-}
